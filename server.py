72a9a0cf69fce483215ce1e03a7c3d93253bf3ff
sambev mac <samb@securitymetrics.com>
2013-05-03 17:10:31 -0600

    ready for the api

--- server.py
+++ server.py
@@ -1,22 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from jconfig import render
-from models.stats import Program
+from models.program import Program
+from models.user import User
 from dbconfig import store
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
     """
     Render the index page and return the list of programs
     """
-    programs = store.find(Program)
-    print programs
-    return render('index.html', {
-        'name': 'Sam', 
-        'programs': programs,
-    })
+    if request.method == 'GET':
+        programs = store.find(Program)
+        users = store.find(User)
+
+        return render('index.html', {
+            'users': users
+        })
+
+
 
 
 if __name__ == "__main__":
